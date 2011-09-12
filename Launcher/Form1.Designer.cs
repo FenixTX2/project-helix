55879c90a944eaaf37930b2812c24fd99b7253e5
FenixTX2 <ts_eye@hotmail.co.uk>
2011-09-12 22:22:40 +0100

    22:22

--- Launcher/Form1.Designer.cs
+++ Launcher/Form1.Designer.cs
@@ -28,21 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.processComboBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // processComboBox
+            // 
+            this.processComboBox.FormattingEnabled = true;
+            this.processComboBox.Location = new System.Drawing.Point(13, 13);
+            this.processComboBox.Name = "processComboBox";
+            this.processComboBox.Size = new System.Drawing.Size(176, 21);
+            this.processComboBox.TabIndex = 0;
+            this.processComboBox.DropDown += new System.EventHandler(this.processComboBox_DropDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 73);
+            this.ClientSize = new System.Drawing.Size(284, 50);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.processComboBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox processComboBox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Launcher/Form1.cs
+++ Launcher/Form1.cs
@@ -6,14 +6,44 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Launcher
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, Process> processDictionary = new Dictionary<string, Process>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            processComboBox_DropDown(null, null);
+        }
+
+        private void processComboBox_DropDown(object sender, EventArgs e)
+        {
+            processComboBox.Items.Clear();
+
+            populateProcessDictionary("wow");
+
+            processComboBox.Items.AddRange(processDictionary.Keys.ToArray<Object>());
+        }
+
+        private void populateProcessDictionary(string processName)
+        {
+            processDictionary.Clear();
+
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            foreach (Process process in processes)
+            {
+                string keyString = process.ProcessName +"<"+process.Id+">";
+                processDictionary.Add(keyString, process); 
+            }
         }
     }
 }

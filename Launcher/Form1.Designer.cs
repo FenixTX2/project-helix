a1360d9c0d6b6b6931d203c451d9b53674a38c8e
FenixTX2 <ts_eye@hotmail.co.uk>
2011-09-12 21:48:20 +0100

    initial commit

--- Launcher/Form1.Designer.cs
+++ Launcher/Form1.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 73);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Launcher";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
